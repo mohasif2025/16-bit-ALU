efd72bb31d5f1e7421fa90621896ee040f5972b2
Mohammad Asif <ahad.asif1973@gmail.com>
2026-05-27 21:47:11 -0400

    Add files via upload

---
+++ alu.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/27/2026 08:54:29 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu16 (
+    input  [15:0] A,
+    input  [15:0] B,
+    input  [1:0] SEL,
+
+    output reg [15:0] RESULT
+);
+
+always @(*) begin
+    case (SEL)
+
+        2'b00: RESULT = A & B;
+
+        2'b01: RESULT = A | B;
+
+        2'b10: RESULT = A ^ B;
+
+        2'b11: RESULT = A + B;
+
+    endcase
+end
+
+endmodule
